af469d55aceaae9d66562683878dcad4098a57c2
Sofiermas <Volserma@yandex.ru>
2023-07-17 00:00:09 +0300

    ref. errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,37 +1,43 @@
 const Card = require('../models/card');
+const { ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_DEFAULT } = require('../utils/errors');
 
 module.exports.createCard = (req, res) => {
   console.log(req.user._id); // _id станет доступен
-
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Некорректные данные.' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: err.message });
+      }
+    });
 };
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => {
       if (cards.length === 0) {
-        res.status(400).send({ message: 'Карточки не найдены.' });
+        res.status(ERROR_NOT_FOUND).send({ message: 'Карточки не найдены.' });
         return;
       }
-      res.send(cards);
+      res.status(200).send(cards);
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(ERROR_DEFAULT).send({ message: err.message }));
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: 'Card not found' });
+        res.status(ERROR_NOT_FOUND).send({ message: 'Card not found' });
         return;
       }
       res.status(200).send(card);
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => res.status(ERROR_DEFAULT).send({ message: err.message }));
 };
 
 module.exports.likeCard = (req, res) => {
@@ -40,8 +46,20 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => {
+      if (card) {
+        res.status(200).send(card);
+      } else {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Card not found' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Некорректные данные.' });
+      } else {
+    res.status(ERROR_DEFAULT).send({ message: err.message });
+  }
+  });
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -50,6 +68,18 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => {
+      if (card){
+    res.status(200).send(card);
+  } else {
+    res.status(ERROR_NOT_FOUND).send({ message: 'Card not found'});
+  }
+  })
+    .catch((err) => {
+      if (err.name === 'CastError'){
+    res.status(ERROR_BAD_REQUEST).send({ message: 'Некорректные данные.' });
+  } else {
+    res.status(ERROR_DEFAULT).send({message: err.message});
+  }
+  });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_DEFAULT } = require('../utils/errors');
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
@@ -6,7 +7,13 @@ const createUser = (req, res) => {
     .then((user) => {
       res.status(200).send(user);
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Некорректные данные.' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: err.message });
+      }
+    });
 };
 
 const getUsers = (req, res) => {
@@ -19,7 +26,7 @@ const getUsers = (req, res) => {
       res.status(200).send(users);
     })
     .catch((err) => {
-      res.status(500).send({ message: err.message });
+      res.status(ERROR_DEFAULT).send({ message: err.message });
     });
 };
 
@@ -27,30 +34,54 @@ const getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: 'User not found' });
+        res.status(ERROR_NOT_FOUND).send({ message: 'User not found' });
         return;
       }
       res.status(200).send(user);
     })
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Некорректные данные.' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: err.message });
+      }
+    });
 };
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id,
     { name, about },
+    { new: true },
   )
     .then((user) => res.status(200).send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'User not found' });
+      } else if (err.name === 'ValidationError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Некорректные данные.' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: err.message });
+      }
+    });
 };
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id,
     { avatar },
+    { new: true },
   )
     .then((user) => res.status(200).send(user))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'User not found' });
+      } else if (err.name === 'ValidationError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Некорректные данные.' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: err.message });
+      }
+    });
 };
 
 module.exports = {
