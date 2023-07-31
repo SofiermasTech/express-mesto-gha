91aa35d53c60891b179cff1dd6a6ff72f3888980
Sofiermas <Volserma@yandex.ru>
2023-08-01 00:42:35 +0300

    test #14.7.3 routes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const mainRouter = require('express').Router();
-const auth = require('../middlewares/auth');
+// const auth = require('../middlewares/auth');
 
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 const NotFoundError = require('../utils/NotFoundError');
 
 // авторизация
-mainRouter.use('/users', auth, userRouter);
-mainRouter.use('/cards', auth, cardRouter);
+mainRouter.use('/users', userRouter);
+mainRouter.use('/cards', cardRouter);
 
-mainRouter.use('*', auth, (req, res, next) => {
+mainRouter.use('*', (req, res, next) => {
   next(new NotFoundError('Запрашиваемая страница не найдена'));
 });
 
